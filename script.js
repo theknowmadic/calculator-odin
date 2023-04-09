eb96f2245a180be84f8e046708a0f96dcece0e6c
theknowmad <127548786+theknowmadic@users.noreply.github.com>
2023-04-09 17:30:38 -0400

    Add eventListener to number buttons

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-const numberButtons = document.querySelectorAll('.btn.number');
-const operationsButtons = document.querySelectorAll('.btn.operator');
-const equalsButton = document.querySelector('.btn.equal');
+const numberButtons = document.querySelectorAll('.number');
+const operationsButtons = document.querySelectorAll('.operator');
+const equalsButton = document.querySelector('.equal');
 const deleteButton = document.querySelector('.delete-btn');
 const allClearButton = document.querySelector('.clear-btn');
 const previousOperandTextElement = document.querySelector('.previous-operand');
 const currentOperandTextElement = document.querySelector('.current-operand');
+
 
 class Calculator {
   constructor(previousOperandTextElement, currentOperandTextElement) {
@@ -12,4 +13,36 @@ class Calculator {
     this.currentOperandTextElement = currentOperandTextElement
     this.clear()
   }
+
+clear() {
+   this.currentOperand = ''
+   this.previousOperand = ''
+   this.operation = undefined
+ }
+
+updateDisplay() {
+  this.currentOperandTextElement.innerText = this.currentOperand;
+  }
+
+
+appendNumber(number) {
+  if (number === '.' && this.currentOperand.includes('.')) return
+  this.currentOperand = this.currentOperand.toString() + number.toString()
+   }
+
 }
+
+const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
+
+ numberButtons.forEach(button => {
+  button.addEventListener('click', (e) => {
+  calculator.appendNumber(button.innerText)
+  calculator.updateDisplay(e)
+    })
+  })
+
+
+
+
+
+
